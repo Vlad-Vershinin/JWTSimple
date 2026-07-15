b4abdb2a010e5731b1ae9c150753e6ab4c2fceb5
Vlad-Vershinin <vladversinin676@gmail.com>
2026-07-15 19:06:56 +0500

    add readme for nuget and fix user interface

--- JWTSimple/Models/UserInterfaces.cs
+++ JWTSimple/Models/UserInterfaces.cs
@@ -4,24 +4,24 @@ namespace JWTSimple.Models;
 
 public interface IAuthUser<TId> where TId : IEquatable<TId>
 {
-    TId Id { get; set; }
-    string Identity { get; set; }
-    string PasswordHash { get; set; }
+    public TId Id { get; set; }
+    public string Identity { get; set; }
+    public string PasswordHash { get; set; }
 
-    IEnumerable<Claim> CustomClaims { get; }
+    public virtual IEnumerable<Claim> CustomClaims => Enumerable.Empty<Claim>();
 }
 
 public interface IEmailAuthenticatable
 {
-    string Email { get; }
+    public string Email { get; }
 }
 
 public interface IUsernameAuthenticatable
 {
-    string Username { get; }
+    public string Username { get; }
 }
 
 public interface IPhoneAuthenticatable
 {
-    string PhoneNumber { get; }
+    public string PhoneNumber { get; }
 }
